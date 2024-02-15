78e54ffdca9357f01fdc8e87ed3d28837ce2cb9a
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-02-15 16:03:09 +0100

    learning continues

---
+++ javascriptLoop/script.js
@@ -0,0 +1,32 @@
+
+    // JAVASCRIPT LOOP
+
+
+    // while loop: 
+
+    let i = 0;
+
+    while (i < 10) {
+        console.log(i);
+        i++;
+    }
+
+    // for loop:
+
+    // for ([initialization]; [codition]; [final-expression]) {
+            // statement
+    // };
+
+    for (let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+
+    for (let i = 1; i <= 8; i++) {
+        console.log(i);
+    };
+
+    const colors = ['red', 'green', 'blue', 'yellow'];
+
+    for (let i = 0; i < colors.length; i++) {
+        console.log(colors[i])
+    }
