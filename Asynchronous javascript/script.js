8d80441651c3c994bd69434a739c603434a7178d
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-03-06 09:21:23 +0100

    js project

--- Asynchronous javascript/script.js
+++ Asynchronous javascript/script.js
@@ -19,7 +19,7 @@ functionOne();
 // CALLBACK FUNCTION
 
 // const fetchUser = (username) => { 
-//     setInterval(() =>  { return {user: username}}, 2000);
+//     setTimeout(() =>  { return {user: username}}, 2000);
 // }
 // const user = fetchUser("test");
 // console.log(user);
@@ -135,8 +135,196 @@ fetchUser("Zainab")
 
 
 
+// async and await
+
+// example1
+const myUSers = {
+    userList: []
+}
+
+const myFoolFunc = async () => {
+   const response = await fetch('https://jsonplaceholder.typicode.com/users');
+   const fetchJson = await response.json();
+//    console.log(fetchJson);
+}
+
+const anotherFn = async () => {
+    const data = await myFoolFunc();
+    myUSers.userList = data;
+    console.log(myUSers.userList);
+
+}
+
+anotherFn()
+// console.log(myUSers.userList);
+
+// example 2
+const getAllUsersEmails = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const fetchJson = await response.json();
+ //    console.log(fetchJson);
+
+    const email = fetchJson.map((user) => {
+        return user.email;
+    })
+    console.log(email)
+ }
+
+getAllUsersEmails();
+
+const getAllUsersNames = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const fetchJson = await response.json();
+ //    console.log(fetchJson);
+
+    const oyee = fetchJson.map((joy) => {
+        return joy.username;
+    })
+    console.log(oyee);
+ }
+
+ getAllUsersNames();
+
+
+//  try, catch and finally block
+
+// SYNTAX
+
+ try {
+
+ } catch (err) {
+
+ }finally {
+
+ }
+
+
+//  example
+
+const getData = async () => {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        return data;
+    }catch (error) {
+        console.log("Error:", error.message)
+    }
+}
+
+getData()
+.then((data) => console.log("data:", data)).catch((error) => console.log("Error:", error.message))
+
+
+const fetchDataWithError = async () => {
+    try {
+        console.log('Fetching Data...')
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!res.ok) {
+            throw new Error ('Failed to fetch data, ori e baje')
+        }
+        const $data = await res.json();
+        return $data;
+
+    }catch (error) {
+        console.log('Error:', error.message)
+    }
+}
+
+fetchDataWithError()
+.then((result) => {
+    if(result) {
+        console.log("Data", result)
+    }
+})
+
+// class work //getting each user's details
+
+const getAUserDetails = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const fetchJson = await response.json();
+ //    console.log(fetchJson);
+    let eachDetail;      
+    const userdetails = [];
+    for (let i = 0; i < fetchJson.length; i++){
+        userdetails.push([fetchJson[i].name, fetchJson[i].username, fetchJson[i].email])
+    } 
+
+    console.log(userdetails);
+ }
+
+ getAUserDetails();
 
 
 
+// another ways
+ const fetchSingleData = async () => {
+    try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  
+      if(!response.ok) {
+        throw new Error('Not found will fixed soon')
+      }
+      
+      const [userData] = await response.json() // assuming data is an array of user
+      const {username, email, name} = userData;
+      return {username, email, name};
+  
+    }catch (error) {
+      console.log('Error:', error.message)
+    }
+  }
+  fetchSingleData().then(({username, email, name}) => {
+    console.log('Username:', username)
+    console.log('Email:', email)
+    console.log('Name:',name)
+  })
 
+//   getting the details of all the 10 users
+
+const fetchAlluserData = async () => {
+    try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  
+      if(!response.ok) {
+        throw new Error('Not found, will fixed soon')
+      }
+      
+      const userData = await response.json() // assuming data is an array of user
+      const mappedData = userData.map(({username, email, name, address, company}) => {
+        address = address['street'];
+        company = company['name']
+      return {username, email, name, address, company}
+      })
+      console.log(mappedData)
+      
+    }catch (error) {
+      console.log('Error:', error.message)
+    }
+  }
+  fetchAlluserData();
+
+
+//   Another ways
+  
+// const fetchAlluserDatas = async () => {
+//     try{
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  
+//       if(!response.ok) {
+//         throw new Error('Not found, will fixed soon')
+//       }
+      
+//       const userData = await response.json() // assuming data is an array of user
+//       const mappedData = userData.map(({id, username, email, name, address, phone, website, company}) => 
+       
+//       console.log(mappedData)
+      
+//     }catch (error) {
+//       console.log('Error:', error.message)
+//     }
+//   }
+//   fetchAlluserDatas();
+
+
+//   mathematicaL METHODS
 
